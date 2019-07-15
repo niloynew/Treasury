ab36a55f6b964438deea3a387a97b5326fc5e95e
zia <zia@mislbd.com>
2019-07-15 10:49:01 +0600

    add query

--- treasury-impl/src/main/java/com/mislbd/ababil/treasury/command/handler/AccountCommandHandlerAggregate.java
+++ treasury-impl/src/main/java/com/mislbd/ababil/treasury/command/handler/AccountCommandHandlerAggregate.java
@@ -1,5 +1,6 @@
 package com.mislbd.ababil.treasury.command.handler;
 
+import com.mislbd.ababil.asset.service.Auditor;
 import com.mislbd.ababil.treasury.command.CreateAccountCommand;
 import com.mislbd.ababil.treasury.command.DeleteAccountCommand;
 import com.mislbd.ababil.treasury.command.UpdateAccountCommand;
@@ -9,9 +10,11 @@ import com.mislbd.ababil.treasury.exception.ProductNotFoundException;
 import com.mislbd.ababil.treasury.mapper.AccountMapper;
 import com.mislbd.ababil.treasury.repository.jpa.AccountRepository;
 import com.mislbd.ababil.treasury.repository.jpa.ProductRepository;
+import com.mislbd.asset.command.api.CommandEvent;
 import com.mislbd.asset.command.api.CommandResponse;
 import com.mislbd.asset.command.api.annotation.Aggregate;
 import com.mislbd.asset.command.api.annotation.CommandHandler;
+import com.mislbd.asset.command.api.annotation.CommandListener;
 import org.springframework.transaction.annotation.Transactional;
 
 @Aggregate
@@ -20,17 +23,25 @@ public class AccountCommandHandlerAggregate {
   private final AccountRepository accountRepository;
   private final AccountMapper accountMapper;
   private final ProductRepository productRepository;
+  private final Auditor auditor;
 
   public AccountCommandHandlerAggregate(
-      AccountRepository accountRepository,
-      AccountMapper accountMapper,
-      ProductRepository productRepository) {
+          AccountRepository accountRepository,
+          AccountMapper accountMapper,
+          ProductRepository productRepository, Auditor auditor) {
     this.accountRepository = accountRepository;
     this.accountMapper = accountMapper;
     this.productRepository = productRepository;
+      this.auditor = auditor;
   }
 
-  @Transactional
+    @CommandListener(
+            commandClasses = {CreateAccountCommand.class, UpdateAccountCommand.class})
+    public void auditChargeCommandListener(CommandEvent e) {
+        auditor.audit(e.getCommand().getPayload(),e.getCommand());
+    }
+
+    @Transactional
   @CommandHandler
   public CommandResponse<Long> createAccount(CreateAccountCommand command) {
     return CommandResponse.of(

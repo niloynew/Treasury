dc74dde724b238d24607607c0ae126e9a665abbb
zia <zia@mislbd.com>
2019-07-15 10:41:34 +0600

    add query

---
+++ treasury-impl/src/main/java/com/mislbd/ababil/treasury/query/AccountQuery.java
@@ -0,0 +1,83 @@
+package com.mislbd.ababil.treasury.query;
+
+import com.mislbd.ababil.treasury.domain.AccountStatus;
+import com.mislbd.asset.query.api.QueryRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+public class AccountQuery extends QueryRequest {
+    private boolean asPage;
+    private Pageable pageable;
+    private Long productId;
+    private String currencyCode;
+    private LocalDate openDate;
+    private LocalDate expiryDate;
+    private  AccountStatus status;
+
+    public AccountQuery(boolean asPage,Pageable pageable, Long productId, String currencyCode, LocalDate openDate, LocalDate expiryDate, AccountStatus status) {
+        this.pageable = pageable;
+        this.productId = productId;
+        this.currencyCode = currencyCode;
+        this.openDate = openDate;
+        this.expiryDate = expiryDate;
+        this.status = status;
+        this.asPage=asPage;
+    }
+
+    public boolean isAsPage() {
+        return asPage;
+    }
+
+    public void setAsPage(boolean asPage) {
+        this.asPage = asPage;
+    }
+
+    public Pageable getPageable() {
+        return pageable;
+    }
+
+    public void setPageable(Pageable pageable) {
+        this.pageable = pageable;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public LocalDate getOpenDate() {
+        return openDate;
+    }
+
+    public void setOpenDate(LocalDate openDate) {
+        this.openDate = openDate;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+}
--- treasury-rs/src/main/java/com/mislbd/ababil/treasury/controller/AccountController.java
+++ treasury-rs/src/main/java/com/mislbd/ababil/treasury/controller/AccountController.java
@@ -9,13 +9,16 @@ import com.mislbd.ababil.treasury.command.DeleteAccountCommand;
 import com.mislbd.ababil.treasury.command.UpdateAccountCommand;
 import com.mislbd.ababil.treasury.domain.Account;
 import com.mislbd.ababil.treasury.domain.AccountStatus;
+import com.mislbd.ababil.treasury.query.AccountQuery;
 import com.mislbd.ababil.treasury.service.AccountService;
 import com.mislbd.asset.command.api.CommandProcessor;
 import com.mislbd.asset.command.api.CommandResponse;
-import com.mislbd.asset.commons.data.domain.PagedResult;
+
 import java.time.LocalDate;
-import java.util.List;
 import javax.validation.Valid;
+
+import com.mislbd.asset.query.api.QueryManager;
+import com.mislbd.asset.query.api.QueryResult;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +30,12 @@ public class AccountController {
 
   private final CommandProcessor commandProcessor;
   private final AccountService accountService;
+    private final QueryManager queryManager;;
 
-  public AccountController(CommandProcessor commandProcessor, AccountService accountService) {
+  public AccountController(CommandProcessor commandProcessor, AccountService accountService, QueryManager queryManager) {
     this.commandProcessor = commandProcessor;
     this.accountService = accountService;
+      this.queryManager = queryManager;
   }
 
   @GetMapping()
@@ -42,16 +47,22 @@ public class AccountController {
       @RequestParam(value = "openDate", required = false) final LocalDate openDate,
       @RequestParam(value = "expiryDate", required = false) final LocalDate expiryDate,
       @RequestParam(value = "status", required = false) final AccountStatus status) {
-    if (asPage) {
+  /*  if (asPage) {
       PagedResult<Account> pagedAccounts =
           accountService.findAccounts(
               pageable, productId, currencyCode, openDate, expiryDate, status);
       return ResponseEntity.ok(pagedAccounts);
     } else {
       List<Account> accounts =
-          accountService.findAccounts(productId, currencyCode, openDate, expiryDate, status);
-      return ResponseEntity.ok(accounts);
-    }
+          accountService.findAccounts(productId, currencyCode, openDate, expiryDate, status);*/
+      QueryResult<?> queryResult=queryManager.executeQuery(new AccountQuery(asPage,
+              pageable,productId,currencyCode,openDate,expiryDate,status));
+      if(queryResult.isEmpty()) {
+          return ResponseEntity.noContent().build();
+      }
+
+      return ResponseEntity.ok(queryResult.getResult());
+
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)

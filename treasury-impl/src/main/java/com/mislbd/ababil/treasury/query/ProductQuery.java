e0ff70d65eafd9a7b5f7455fb95c7ae62360d37b
Syed Miraj <miraj@mislbd.com>
2019-07-17 17:39:40 +0600

    add lombok in product and account query

--- treasury-impl/src/main/java/com/mislbd/ababil/treasury/query/ProductQuery.java
+++ treasury-impl/src/main/java/com/mislbd/ababil/treasury/query/ProductQuery.java
@@ -1,8 +1,17 @@
 package com.mislbd.ababil.treasury.query;
 
 import com.mislbd.asset.query.api.QueryRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.Pageable;
 
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductQuery extends QueryRequest {
 
   private boolean asPage;
@@ -10,31 +19,9 @@ public class ProductQuery extends QueryRequest {
   private String name;
   private String code;
 
-  public ProductQuery(boolean asPage, Pageable pageable, String name, String code) {
-    this.asPage = asPage;
-    this.pageable = pageable;
-    this.name = name;
-    this.code = code;
-  }
-
   public ProductQuery(String name, String code) {
     this.name = name;
     this.code = code;
   }
 
-  public boolean isAsPage() {
-    return asPage;
-  }
-
-  public Pageable getPageable() {
-    return pageable;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getCode() {
-    return code;
-  }
 }

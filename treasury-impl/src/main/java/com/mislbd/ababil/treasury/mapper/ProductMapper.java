b0b7e40499a8ecaa2b28d20aa1a13fddc2364853
Syed Miraj <miraj@mislbd.com>
2019-07-18 12:49:51 +0600

    update GL mapping with product

--- treasury-impl/src/main/java/com/mislbd/ababil/treasury/mapper/ProductMapper.java
+++ treasury-impl/src/main/java/com/mislbd/ababil/treasury/mapper/ProductMapper.java
@@ -3,10 +3,13 @@ package com.mislbd.ababil.treasury.mapper;
 import com.mislbd.ababil.treasury.domain.Product;
 import com.mislbd.ababil.treasury.exception.ProductNatureNotFoundException;
 import com.mislbd.ababil.treasury.repository.jpa.ProductNatureRepository;
+import com.mislbd.ababil.treasury.repository.jpa.ProductRelatedGLRepository;
 import com.mislbd.ababil.treasury.repository.jpa.ProductRepository;
 import com.mislbd.ababil.treasury.repository.schema.ProductEntity;
 import com.mislbd.asset.commons.data.domain.ResultMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class ProductMapper {
@@ -14,14 +17,18 @@ public class ProductMapper {
   private final ProductRepository productRepository;
   private final ProductNatureRepository productNatureRepository;
   private final ProductNatureMapper productNatureMapper;
+  private final ProductRelatedGLRepository productRelatedGLRepository;
+  private final ProductGLMapper productGLMapper;
 
   public ProductMapper(
-      ProductRepository productRepository,
-      ProductNatureRepository productNatureRepository,
-      ProductNatureMapper productNatureMapper) {
+          ProductRepository productRepository,
+          ProductNatureRepository productNatureRepository,
+          ProductNatureMapper productNatureMapper, ProductRelatedGLRepository productRelatedGLRepository, ProductGLMapper productGLMapper) {
     this.productRepository = productRepository;
     this.productNatureRepository = productNatureRepository;
     this.productNatureMapper = productNatureMapper;
+    this.productRelatedGLRepository = productRelatedGLRepository;
+    this.productGLMapper = productGLMapper;
   }
 
   public ResultMapper<ProductEntity, Product> entityToDomain() {
@@ -34,8 +41,12 @@ public class ProductMapper {
             .setProfitApplicable(entity.isProfitApplicable())
             .setProductNatureId(entity.getProductNature().getId())
             .setProfitCalculationMethod(entity.getProfitCalculationMethod())
+            .setStatus(entity.getStatus())
             .setDaysInYear(entity.getDaysInYear())
-            .setStatus(entity.getStatus());
+                .setProductGeneralLedgerMappingList(
+                        productRelatedGLRepository.findAllByProductId(entity.getId()).stream()
+                        .map(productRelatedGLEntity -> productGLMapper.entityToDomain().map(productRelatedGLEntity))
+                        .collect(Collectors.toList()));
   }
 
   public ResultMapper<Product, ProductEntity> domainToEntity() {
--- treasury-impl/src/main/java/com/mislbd/ababil/treasury/repository/jpa/ProductRelatedGLRepository.java
+++ treasury-impl/src/main/java/com/mislbd/ababil/treasury/repository/jpa/ProductRelatedGLRepository.java
@@ -1,8 +1,11 @@
 package com.mislbd.ababil.treasury.repository.jpa;
 
 import com.mislbd.ababil.treasury.repository.schema.ProductRelatedGLEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ProductRelatedGLRepository
-    extends JpaRepository<ProductRelatedGLEntity, Long>, JpaSpecificationExecutor {}
+    extends JpaRepository<ProductRelatedGLEntity, Long>, JpaSpecificationExecutor {
+  List<ProductRelatedGLEntity> findAllByProductId(long productId);
+}

c0675f5ca413c2eb212231621af78a92758b5ca9
Syed Miraj <miraj@mislbd.com>
2019-10-16 14:04:46 +0600

    work on reactive account

--- treasury-impl/src/main/java/com/mislbd/ababil/treasury/command/handler/AccountCommandHandlerAggregate.java
+++ treasury-impl/src/main/java/com/mislbd/ababil/treasury/command/handler/AccountCommandHandlerAggregate.java
@@ -46,7 +46,7 @@ public class AccountCommandHandlerAggregate {
   }
 
   @CommandListener(
-      commandClasses = {CreateTreasuryAccountCommand.class, UpdateTreasuryAccountCommand.class})
+      commandClasses = {CreateTreasuryAccountCommand.class, UpdateTreasuryAccountCommand.class, SettlementOrCloseTreasuryAccountCommand.class, ReactivateTreasuryAccountCommand.class})
   public void auditAccountCreateAndUpdate(CommandEvent e) {
     auditor.audit(e.getCommand().getPayload(), e.getCommand());
   }
